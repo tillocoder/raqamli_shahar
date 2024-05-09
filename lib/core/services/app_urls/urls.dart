82b8f090c73071b27b750a10f10d4be23d8a1a1c
tillo <tillo4079@gmail.com>
2024-05-08 22:13:57 -0700

    fixing for tillo

--- lib/core/services/app_urls/urls.dart
+++ lib/core/services/app_urls/urls.dart
@@ -2,11 +2,12 @@ sealed class Urls {
   static const baseUrl = 'http://mahalla.ijro-app.uz';
   static const apiToken = '/api/token/';
   static const apiRefresh = '/api/token/refresh/';
-  static const apiActivities = '/api/v1/citizen/activities/'; //! :id/
+  static const apiActivities = '/api/v1/citizen/activities/';
   static const apiactivitiescreate = '/api/v1/citizen/activities/create/';
   static const apiCitizenCreate = '/api/v1/citizen/create/';
   static const apiCitizenList = '/api/v1/citizen/list/';
-  static const apiCitizenUpdate = '/api/v1/citizen/update/'; //! :id/
+  static const apiCitizenSearch = '/api/v1/citizen/search/';
+  static const apiCitizenUpdate = '/api/v1/citizen/update/';
   static const apiDirectionList = '/api/v1/direction/list/';
   static const apiSearchCitizen =
       'http://mahalla.ijro-app.uz/api/v1/citizen/search/?q=sardor';
