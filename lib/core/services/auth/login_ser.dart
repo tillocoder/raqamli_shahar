dc42d2d51a0b642ba5af31987e7449300926072f
tillo <tillo4079@gmail.com>
2024-05-09 01:03:20 -0700

    fixing

--- lib/core/services/auth/login_ser.dart
+++ lib/core/services/auth/login_ser.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -9,19 +11,20 @@ import 'package:tamorqa_app/setup.dart';
 
 class AuthLoginService {
   static Dio dio = Dio(Baseoption.baseOptions);
-  static Future<void> getToken(Map<String, String> data, BuildContext context) async {
+  static Future<void> getToken(
+      Map<String, String> data, BuildContext context) async {
     try {
-      var response = await dio.post('${Urls.baseUrl}${Urls.apiToken}', data: data);
+      var response =
+          await dio.post('${Urls.baseUrl}${Urls.apiToken}', data: data);
       if (response.statusCode == 200 || response.statusCode == 201) {
         var responseData = response.data as Map<String, dynamic>;
         String? accessToken = responseData["access"] as String?;
-        String? refreshToken = responseData["access"] as String?;
+        String? refreshToken = responseData["refresh"] as String?; // fix here
         if (accessToken != null) {
           debugPrint("Access Token: $accessToken");
           debugPrint("Refresh Token: $refreshToken");
           await box.put('access', accessToken);
           await box.put('refresh', refreshToken);
-          // ignore: use_build_context_synchronously
           context.go(Routes.home);
         } else {
           return;
@@ -31,10 +34,72 @@ class AuthLoginService {
         return;
       }
     } catch (e) {
-      // ignore: use_build_context_synchronously
-      SnackBarHelper.showInternetConnectionError(context, 'Kirish uchun ruxsat yo\'q');
+      SnackBarHelper.showInternetConnectionError(
+        context,
+        'Kirish uchun ruxsat yo\'q',
+      );
       debugPrint('token ololmadi tasdiqlanmagan user');
       return;
     }
+  }
+
+  static Future<String?> getAccessToken() async {
+    // Function to retrieve the access token from wherever you've stored it
+    return box.get('access');
+  }
+
+  static Future<String?> getRefreshToken() async {
+    // Function to retrieve the refresh token from wherever you've stored it
+    return box.get('refresh');
+  }
+
+  static Future<void> refreshTokenIfNeeded() async {
+    String? accessToken = await getAccessToken();
+    if (accessToken != null) {
+      // Check if the access token is expired
+      if (JwtUtils.isExpired(accessToken)) {
+        // Access token is expired, refresh it
+        String? refreshToken = await getRefreshToken();
+        if (refreshToken != null) {
+          try {
+            // Send a request to refresh the access token
+            var response = await dio.post('${Urls.baseUrl}${Urls.apiRefresh}',
+                data: {"refresh": refreshToken});
+            if (response.statusCode == 200 || response.statusCode == 201) {
+              var responseData = response.data as Map<String, dynamic>;
+              String? newAccessToken = responseData["access"] as String?;
+              if (newAccessToken != null) {
+                await box.put('access', newAccessToken);
+              }
+            }
+          } catch (e) {
+            // Handle error while refreshing token
+            debugPrint('Error refreshing token: $e');
+          }
+        }
+      }
+    }
+  }
+}
+
+class JwtUtils {
+  static bool isExpired(String token) {
+    final parts = token.split('.');
+    if (parts.length != 3) {
+      throw Exception('Invalid token');
+    }
+    final payload = parts[1];
+    final decoded = Uri.tryParse(Uri.decodeComponent(payload))?.query;
+    if (decoded == null) {
+      throw Exception('Invalid payload');
+    }
+    final data = json.decode(decoded) as Map<String, dynamic>;
+    final exp = data['exp'] as int?;
+    if (exp == null) {
+      throw Exception('Expiration not found');
+    }
+    final expirationDate = DateTime.fromMillisecondsSinceEpoch(exp * 1000);
+    final now = DateTime.now();
+    return now.isAfter(expirationDate);
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,17 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
+import firebase_messaging
+import flutter_secure_storage_macos
 import geolocator_apple
 import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
