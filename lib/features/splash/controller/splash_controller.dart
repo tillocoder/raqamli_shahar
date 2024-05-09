66b6c4f7cc9e866294f3cf83c8a7c6159c3e3fcf
tillo <tillo4079@gmail.com>
2024-05-08 22:36:35 -0700

    formatting for male and female

---
+++ lib/features/splash/controller/splash_controller.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+//
+// final splashCtr = ChangeNotifierProvider.autoDispose(
+//   (ref) => SplashController(),
+// );
+//
+// class SplashController extends ChangeNotifier {
+//   bool isLoading = false;
+//   CitizenAddController() {
+//     init();
+//   }
+// }
